c9a39c21409f1d42617033683814b79254b9d930
Emil Imbert Villumsen <emimvi@gmail.com>
2019-10-20 00:22:39 +0200

    Print result csv to stdout, get input file as argument

--- underlords.sh
+++ underlords.sh
@@ -1,13 +1,20 @@
 #! /usr/bin/env python3
 import csv
+import sys
 from collections import defaultdict
+
+if len(sys.argv) != 2:
+    print('Need a heroes.csv file argument')
+    exit()
+
+input_file = sys.argv[1]
 
 Table = defaultdict(lambda: defaultdict(list))
 Classes = set()
 
 # Read into [Race][Class]=Heroes dictionary
 # Keeping track of Classes to be used as header
-with open('./heroes.csv', newline='') as csvfile:
+with open(input_file, newline='') as csvfile:
     parser = csv.DictReader(csvfile, delimiter=',')
     for row in parser:
         c = row['Class']
@@ -23,13 +30,12 @@ with open('./heroes.csv', newline='') as csvfile:
         Classes |= {c, c2}
 
 # Write to csv, converting Heroes lists to strings seperated by newline
-with open('./res.csv','w+', newline='') as csvfile:
-    w = csv.DictWriter(csvfile, sorted(Classes) )
-    w.writeheader()
-    for key,val in (sorted(Table.items())):
-        strdict = {}
-        for k,v in val.items():
-            strdict[k] = '\n'.join(v)
-        row = {'': key}
-        row.update(strdict)
-        w.writerow(row)
+w = csv.DictWriter(sys.stdout, sorted(Classes) )
+w.writeheader()
+for key,val in (sorted(Table.items())):
+    strdict = {}
+    for k,v in val.items():
+        strdict[k] = '\n'.join(v)
+    row = {'': key}
+    row.update(strdict)
+    w.writerow(row)
